c96e523df9b91af83fb96151262fbfafe831fd02
MasterD3204 <22022561@vnu.edu.vn>
2026-03-24 11:25:33 +0700

    fix: remove ndk.abiFilters conflicting with splits.abi config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,10 +18,6 @@ android {
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        ndk {
-            abiFilters += listOf("arm64-v8a", "x86_64")
-        }
     }
 
     buildTypes {
